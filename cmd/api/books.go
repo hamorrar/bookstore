bfcf065459476f0cb8cf8fd5583b2dfa43321233
Hilal Morrar <hilalmorrar@gmail.com>
2025-08-29 00:12:33 -0700

    added routes and basic CRUD functionality for books

---
+++ cmd/api/books.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hamorrar/bookstore/internal/database"
+)
+
+func (app *application) createBook(c *gin.Context) {
+	var book database.Book
+
+	if err := c.ShouldBindJSON(&book); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err := app.models.Books.CreateBook(&book)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create book"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, book)
+}
+
+func (app *application) getAllBooks(c *gin.Context) {
+	books, err := app.models.Books.GetAllBooks()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get all books"})
+		return
+	}
+	c.JSON(http.StatusOK, books)
+}
+
+func (app *application) getBook(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid book ID"})
+		return
+	}
+
+	book, err := app.models.Books.GetBook(id)
+	if book == nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Book not found"})
+		return
+	}
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get book"})
+		return
+	}
+
+	c.JSON(http.StatusOK, book)
+
+}
+
+func (app *application) deleteBook(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid book ID"})
+		return
+	}
+
+	if err := app.models.Books.DeleteBook(id); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete book"})
+		return
+	}
+
+	c.JSON(http.StatusNoContent, nil)
+}
--- cmd/api/router.go
+++ cmd/api/router.go
@@ -8,25 +8,36 @@ import (
 
 func (app *application) routes() http.Handler {
 	g := gin.Default()
+
+	v1 := g.Group("/api/v1")
+
+	{
+		v1.GET("/ping", ping)
+
+		v1.POST("/books", app.createBook)
+		v1.GET("/books", app.getAllBooks)
+		v1.GET("/books/:id", app.getBook)
+		v1.DELETE("/books/:id", app.deleteBook)
+	}
 	return g
 }
 
-func InitRouter(router *gin.Engine) {
-	// Set up router and routes
-	router.GET("/api/v1/ping", ping)
-	// router.POST("/api/v1/login", auth.Login)
-	router.GET("/", homepage)
-	// router.POST("/api/v1/Books/toggle", books.Toggle)
-	// router.POST("/api/v1/Books/add", books.Add(db))
-}
+// func InitRouter(router *gin.Engine) {
+// 	// Set up router and routes
+// 	router.GET("/api/v1/ping", ping)
+// 	// router.POST("/api/v1/login", auth.Login)
+// 	// router.GET("/", homepage)
+// 	// router.POST("/api/v1/Books/toggle", books.Toggle)
+// 	// router.POST("/api/v1/Books/add", books.Add(db))
+// }
 
 func ping(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "version": "v1"})
 }
 
-func homepage(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", gin.H{
-		"LoggedIn": "",
-		"Username": "",
-	})
-}
+// func homepage(c *gin.Context) {
+// 	c.HTML(http.StatusOK, "index.html", gin.H{
+// 		"LoggedIn": "",
+// 		"Username": "",
+// 	})
+// }
