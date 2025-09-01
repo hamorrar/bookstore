936247e38df4f53a8c0fe5e112a67ce90a73fd69
Hilal Morrar <hilalmorrar@gmail.com>
2025-09-01 07:18:16 -0700

    renamed an order sql column

--- cmd/migrate/migrations/3_create_orders_table.up.sql
+++ cmd/migrate/migrations/3_create_orders_table.up.sql
@@ -2,6 +2,6 @@ create table if not exists orders (
     order_id serial primary key,
     order_user_id serial not null,
     order_status varchar(256) not null,
-    order_totalPrice int not null,
+    order_total_price int not null,
     constraint fk_user foreign key (order_user_id) references users(user_id)
 );
