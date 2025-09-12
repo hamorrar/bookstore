17f37027a1cad610a567681a25bbea9dfeeabb30
Hilal Morrar <hilalmorrar@gmail.com>
2025-09-12 01:40:37 -0700

    removed global

--- cmd/api/main_test.go
+++ cmd/api/main_test.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"strconv"
 
 	"github.com/hamorrar/bookstore/internal/testutils"
 )
-
-var BASE_URL_v1 string = fmt.Sprintf("http://%s:%s/api/v1", os.Getenv("HOST"), os.Getenv("PORT"))
 
 func SetupTest() *application {
 	models := testutils.SetupDB()
