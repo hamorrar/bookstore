4f0f2c337049db4a13ddb7297828037e472e4737
Hilal Morrar <hilalmorrar@gmail.com>
2025-09-12 19:32:35 -0700

    fixed a bug in table set up

--- cmd/migrate/migrations/3_create_orders_table.up.sql
+++ cmd/migrate/migrations/3_create_orders_table.up.sql
@@ -1,6 +1,6 @@
 create table if not exists orders (
     order_id serial unique primary key,
-    order_user_id serial not null,
+    order_user_id int not null,
     order_status varchar(256) not null,
     order_total_price int not null,
     foreign key (order_user_id) references users(user_id) on delete cascade
