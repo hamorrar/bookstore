b03c4b52e7a80dfc3cedb6186bd45fe60df5d66b
Hilal Morrar <hilalmorrar@gmail.com>
2025-09-13 02:27:56 -0700

    removed authorization header from middleware

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"net/http"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
@@ -10,25 +9,14 @@ import (
 
 func (app *application) AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		authHeader := c.GetHeader("Authorization")
-		var tokenString string
-
-		if strings.HasPrefix(authHeader, "Bearer ") {
-			tokenString = strings.TrimPrefix(authHeader, "Bearer ")
-		} else {
-			cookie, err := c.Cookie("auth_token")
-			if err == nil {
-				tokenString = cookie
-			}
-		}
-
-		if tokenString == "" {
+		cookie, err := c.Cookie("auth_token")
+		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Missing token"})
 			c.Abort()
 			return
 		}
 
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		token, err := jwt.Parse(cookie, func(token *jwt.Token) (interface{}, error) {
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 				return nil, jwt.ErrSignatureInvalid
 			}
